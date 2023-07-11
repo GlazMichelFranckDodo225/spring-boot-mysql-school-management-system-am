b218663c04fa07139e0a40f9660b7b91bdf4a0d6
FranckDodo91 <devlogappdgmf@gmail.com>
2023-07-11 14:46:55 +0200

    Project initialization

---
+++ src/test/java/com/dgmf/springbootmysqlsms/SpringBootMysqlSchoolManagementSystemAmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgmf.springbootmysqlsms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMysqlSchoolManagementSystemAmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
