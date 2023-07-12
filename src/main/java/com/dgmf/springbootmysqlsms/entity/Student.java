d2a913ebaa6899437c575a218fa0d5e3f3435031
FranckDodo91 <devlogappdgmf@gmail.com>
2023-07-12 07:50:33 +0200

    Creation of the "Student" entity

---
+++ src/main/java/com/dgmf/springbootmysqlsms/entity/Student.java
@@ -0,0 +1,21 @@
+package com.dgmf.springbootmysqlsms.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "students")
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+public class Student {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Date dob;
+}
